9e20b03c7b1e01d86b46ae2489b892bedd0ba347
Lixun-s <84944212+Lixun-s@users.noreply.github.com>
2021-06-25 16:53:03 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-export let token: string = "ODMxNTM4MDM2OTYxMTgxNzQ3.YHWsJQ.tt26mIrOOL0HXd2rkyHC01TqnTk";
+export let token: string = "";
 export let prefix: string = '/';
 export let owners: Array<string> = [];
 export let dbName: string = "TestDB";
 export let version: string = "1.0.0-beta01";
-export let mongoURI: string = "mongodb+srv://LukasW:VqGxMGqcbB4Dsb5b@mongodb-luner.q9gis.mongodb.net/yeye?retryWrites=true&w=majority";
+export let mongoURI: string = "";
